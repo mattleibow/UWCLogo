7c73d6ae2bd249d47dcb9ee7811eefbe4207e26f
Matthew Leibowitz <mattleibow@live.com>
2024-03-07 10:50:31 +0200

    Add initial code
    Fixes #1

---
+++ UWCLogo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UWCLogo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
