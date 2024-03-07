7e0358b27473c987d7cd7e4c343a6c7add47707f
Matthew Leibowitz <mattleibow@live.com>
2024-03-07 11:45:26 +0200

    Add cool things
    Fixes #3
    Fixes #4

--- UWCLogo/MauiProgram.cs
+++ UWCLogo/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace UWCLogo
 {
@@ -9,6 +10,7 @@ namespace UWCLogo
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseSkiaSharp()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
